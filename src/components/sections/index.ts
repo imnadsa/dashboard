c8434397c0b9bc076f8272f7f12aaf83d5d6040d
imnadsa <antosh1109@mail.ru>
2026-01-26 23:32:01 +0400

    Create index.ts

---
+++ src/components/sections/index.ts
@@ -0,0 +1,7 @@
+export { default as BalancesSection } from './BalancesSection';
+export { default as StatsSection } from './StatsSection';
+export { default as AveragesSection } from './AveragesSection';
+export { default as ProfitChart } from './ProfitChart';
+export { default as ExpenseAnalysis } from './ExpenseAnalysis';
+export { default as IncomeAnalysis } from './IncomeAnalysis';
+export { default as DailyRevenueChart } from './DailyRevenueChart';
