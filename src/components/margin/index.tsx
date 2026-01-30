7b7874e8a02106236a971d40bc1e97b413728f36
imnadsa <antosh1109@mail.ru>
2026-01-31 03:26:25 +0400

    Update index.tsx

--- src/components/margin/index.tsx
+++ src/components/margin/index.tsx
@@ -3,3 +3,6 @@ export { default as ServiceCalculator } from './ServiceCalculator';
 export { default as ExpenseBlock } from './ExpenseBlock';
 export { default as MarginGradient } from './MarginGradient';
 export { default as NewPriceBlock } from './NewPriceBlock';
+export { default as MarginHeader } from './MarginHeader';
+export { default as MarginSummary } from './MarginSummary';
+export { default as MarginSummaryRow } from './MarginSummaryRow';
