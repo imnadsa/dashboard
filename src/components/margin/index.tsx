67678b5ca82e256dcfd7bec092127b9b94d74390
imnadsa <antosh1109@mail.ru>
2026-01-30 21:58:56 +0400

    Create index.tsx

---
+++ src/components/margin/index.tsx
@@ -0,0 +1,5 @@
+export { default as ServicesList } from './ServicesList';
+export { default as ServiceCalculator } from './ServiceCalculator';
+export { default as ExpenseBlock } from './ExpenseBlock';
+export { default as MarginGradient } from './MarginGradient';
+export { default as NewPriceBlock } from './NewPriceBlock';
